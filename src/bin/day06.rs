b2c2669385347c0a1d9da8fe0e6a2b8dca88875d
Geno Nullfree <nullfree.geno@gmail.com>
2022-12-07 17:42:09 -0500

    day06 part 1

---
+++ src/bin/day06.rs
@@ -0,0 +1,37 @@
+const INPUT: &str = include_str!("../../day06.txt");
+
+fn main() {
+    let mut window = Vec::<char>::new();
+    window.resize(4, '0');
+    for (n, i) in INPUT.chars().enumerate() {
+        if n < 4 {
+            shift_in(&mut window, i);
+        }
+        if are_unique(&window) {
+            println!("marker at: {}", n);
+            break;
+        }
+        shift_in(&mut window, i);
+    }
+}
+
+fn shift_in(window: &mut Vec<char>, input: char) {
+    window.remove(0);
+    window.push(input);
+}
+
+fn are_unique(window: &Vec<char>) -> bool {
+    for i in window {
+        let mut count = 0;
+        for c in window {
+            if i == c {
+                count += 1;
+            }
+        }
+        if count != 1 {
+            return false;
+        }
+    }
+
+    true
+}
